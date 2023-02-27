3ed94f4f5938724af33b70c468e01cf2f3434bc2
lilia <lilianom59@outlook.fr>
2023-02-27 05:35:43 +0100

    jointures

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
  * 2. Connectez vous à la base de données blog.
  */
 
+$server = 'localhost';
+$db = 'blog';
+$user = 'root';
+$pwd = '';
+
+try {
+    $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
+
 /**
  * 3. Sans utiliser les alias, effectuez une jointure de type INNER JOIN de manière à récupérer :
  *   - Les articles :
@@ -18,6 +32,17 @@
 
 // TODO Votre code ici.
 
+$request = $bdd->prepare("
+    SELECT article.id, article.title, article.content, categorie.name
+    FROM article
+    INNER JOIN categorie ON article.category_fk = categorie.id
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fetchAll());
+echo "</pre><br>";
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
@@ -25,6 +50,17 @@
 
 // TODO Votre code ici.
 
+$request = $bdd->prepare("
+    SELECT ar.id, ar.title, ar.content, ca.name
+    FROM article as ar
+    INNER JOIN categorie as ca ON ar.category_fk = ca.id
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fetchAll());
+echo "</pre><br>";
 
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
@@ -33,3 +69,27 @@
  */
 
 // TODO Votre code ici.
+
+$NewUser = $bdd->prepare("INSERT INTO utilisateur VALUES (null, 'Lilian', 'Vanhoorne', 'lilianom59@outlook.fr', 'MDPFullSecure123')");
+
+$NewUser->execute();
+
+$NewUser = $bdd->prepare("
+    INSERT INTO commentaire VALUES (null, 'Je suis un super commentaire !', 1, 1),
+                                   (null, 'Je suis également un super commentaire !', 15, 2)
+                               ");
+
+$NewUser->execute();
+
+$request = $bdd->prepare("
+    SELECT com.content, user.firstName, user.lastName
+    FROM commentaire as com
+    LEFT JOIN utilisateur as user ON user.id = com.user_fk
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fecthAll());
+echo "</pre><br>";
+
